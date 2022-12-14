d3dbad7fad73ea98866f05506fc9fcc99686866e
Tugba Borekci <tugbaborekci80@gmail.com>
2022-12-14 15:33:37 +0000

    Created Toyota class

--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -1,6 +1,23 @@
 package day38_Inheritance.carTask;
 
-public class Toyota {
+public class Toyota extends Car {
+
+    public Toyota(String model, int year, double price, String color, int mile) {
+        super("Toyota", model, year, price, color, mile);
+
+
+    }
+    public void reliable(){
+
+        System.out.println(brand+" "+model+" is reliable");
+    }
 
 
 }
+
+/*
+1. Toyota:
+					extra methods:
+							reliabile()
+
+ */
