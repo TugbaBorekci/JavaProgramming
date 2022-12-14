34e030fbc4c68f95070483feed49c0dc4707d623
Tugba Borekci <tugbaborekci80@gmail.com>
2022-12-14 15:45:57 +0000

    Created BMW class

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,25 @@
+package day38_Inheritance.carTask;
+
+public class BMW extends Car{
+
+    public BMW( String model, int year, double price, String color, int mile) {
+        super("BMW", model, year, price, color, mile);
+
+    }
+
+    public void breaksDown(){
+        System.out.println(brand+" "+model+" is breaking down");
+    }
+
+    public void racing(){
+        System.out.println(brand + " "+ model +" is a racing car");
+    }
+}
+
+
+/*
+2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+ */
