d187aa76c0768d87b6fcf37348565cf24f619924
Tugba Borekci <tugbaborekci80@gmail.com>
2022-12-14 15:02:41 +0000

    Created car classes

--- src/day38_Inheritance/Car.java
+++ src/day38_Inheritance/Car.java
@@ -2,4 +2,59 @@ package day38_Inheritance;
 
 public class Car {
 
+    public String brand, model;
+    public int year;
+    public double price;
+    public String color;
+    public int mile;
+
+    public Car(String brand, String model, int year, double price, String color, int mile) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.mile = mile;
+    }
+
+    public void start(){
+        System.out.println(brand+" "+ model+ " is starting ");
+    }
+
+    public void drive(){
+        System.out.println(brand+" "+ model+ " is driving ");
+    }
+
+    /*
+    public void fly(){
+
+    }
+
+    fly() is not common for all the cars, and parent class should only contain the common feature of all subclasses
+     */
+
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", mile=" + mile +
+                '}';
+    }
 }
+
+
+
+/*
+Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+
+				add a constructor to set all the fields
+
+			instance methods:
+				start(), drive(),toString()
+ */
