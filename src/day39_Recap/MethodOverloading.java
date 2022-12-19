9f4d3b82b591f1fac297554e1feda019ea04b316
Tugba Borekci <tugbaborekci80@gmail.com>
2022-12-19 09:09:05 +0000

    day39: Recap

---
+++ src/day39_Recap/MethodOverloading.java
@@ -0,0 +1,12 @@
+package day39_Recap;
+
+public class MethodOverloading {
+
+    private void method1(){
+
+    }
+
+    private int method1(int a){//-->parameter must be different.
+        return  a;
+    }
+}
