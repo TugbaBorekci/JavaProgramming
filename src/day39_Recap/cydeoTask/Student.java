f939511dffea65c3c7f4438b44cd030bcf7c3947
Tugba Borekci <tugbaborekci80@gmail.com>
2022-12-19 20:58:25 +0000

    day39: Recap

---
+++ src/day39_Recap/cydeoTask/Student.java
@@ -0,0 +1,56 @@
+package day39_Recap.cydeoTask;
+
+public class Student extends Person {
+
+    private int studentId;
+    private String fieldOfStudy;
+
+    //create constructor
+    public Student(String name, int age, char gender, int studentId, String fieldOfStudy) {
+        super(name, age, gender);
+        setStudentId(studentId);
+        setFieldOfStudy(fieldOfStudy);
+    }
+
+    //create getter & setter
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+
+    public void study(){
+        System.out.println(getName()+" is studying");
+    }
+    //create toString method
+    public String toString() {
+        return "Student{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", studentId=" + studentId +
+                ", fieldOfStudy='" + fieldOfStudy + '\'' +
+                '}';
+    }
+}
+
+/*
+6. Create a sub class of Person named Student
+            Extra variables:
+                studentId, fieldOfStudy
+            Encapsulate all the fields
+            Add a constructor to set all the fields
+            Encapsulate all the fields
+            Methods:
+                study()
+ */
