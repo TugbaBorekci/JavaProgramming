d8fce7cd1cc5c38c8e07e9940ce592adf371601b
Tugba Borekci <tugbaborekci80@gmail.com>
2022-12-19 10:01:50 +0000

    day39: Recap

--- src/day39_Recap/shapeaTask/Shape.java
+++ src/day39_Recap/shapeaTask/Shape.java
@@ -33,8 +33,14 @@ public class Shape {
         return 0;
     }
 
-
-
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
 /*
 Shape:
---
+++ src/day39_Recap/shapeaTask/Square.java
@@ -0,0 +1,21 @@
+package day39_Recap.shapeaTask;
+
+public class Square {
+
+
+
+
+}
+/*
+Square extends Shape:
+	variables:
+		side;
+
+	Encapsulate the field
+
+	Add a constructor to set the filed
+
+	area(): side * side
+	perimeter(): side * 4
+	toString(): side, area, perimeter
+ */
