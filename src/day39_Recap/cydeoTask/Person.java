38cd6145e8645a560db11f5afb40f16eb0ccd349
Tugba Borekci <tugbaborekci80@gmail.com>
2022-12-19 18:55:37 +0000

    day39: Recap

---
+++ src/day39_Recap/cydeoTask/Person.java
@@ -0,0 +1,78 @@
+package day39_Recap.cydeoTask;
+
+public class Person {
+
+    //first crate instance variables
+    private String name;
+    private int age;
+    private char gender;
+
+    //create generate right click getter & setter all variables(name,age,gender)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    //create construct
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public void eat(){
+        System.out.println(name+" is eating");
+    }
+
+    public void drink(){
+        System.out.println(name+" is drinking");
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+    //create toString method
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
+/*
+1. Create a class named Person:
+            Variables:
+                name, age, gender
+
+            Encapsulate all the fields
+
+            Add a constructor to set all the fields
+
+            Methods:
+                eat()
+                drink()
+                sleep()
+                toString()
+ */
